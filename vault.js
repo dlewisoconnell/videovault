cce7f7307278b9fef79caf3365bac4a033517ea7
dlewisoconnell <dlewisoconnell@gmail.com>
2022-11-07 20:07:34 -0500

    updated menu accessability

--- vault.js
+++ vault.js
@@ -13,34 +13,34 @@ const getNextIdx = (idx = 0, length, direction) => {
 let idx; 
 const getNewIndexAndRender = (direction) => {
      idx = getNextIdx(idx, length, direction);
-     result.innerHTML = '<div class="square"><div class="one"><a href="/movies/' 
+     result.innerHTML = '<ul class="ul2"><div class="square"><li class="one"><a href="/movies/' 
      + genres[idx] + '/01.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/01.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/01.jpg" width=70px alt="Video 1"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/02.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/02.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/02.jpg" width=70p alt="Video 2"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/03.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/03.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/03.jpg" width=70px alt="Video 3"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/04.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/04.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/04.jpg" width=70px alt="Video 4"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/05.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/05.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/05.jpg" width=70px alt="Video 5"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/06.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/06.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/06.jpg" width=70px alt="Video 6"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/07.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/07.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/07.jpg" width=70px alt="Video 7"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/08.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/08.jpg" width=70px></a></div><div class="one"><a href="/movies/' 
+     '/small/08.jpg" width=70px alt="Video 8"></a></li><li class="one"><a href="/movies/' 
      + genres[idx] + '/09.html"><img src="/movies/' 
      + genres[idx] + 
-     '/small/09.jpg" width=70px></a></div></div>'
+     '/small/09.jpg" width=70px alt="Video 9"></a></li></ul></div>'
 
      document.getElementById("genreName").innerHTML = '<h1>' + genres[idx] + '</h1>';
 
